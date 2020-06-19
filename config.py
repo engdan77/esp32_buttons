0713a0fe66892ada836402fc00c53844254bd308
Daniel Engvall <daniel@engvalls.eu>
2020-06-19 08:46:48 +0200

    Minor fixes

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from images import audio, poop, tv
+from ucollections import OrderedDict
 
 buttons_conf_other = {
     1: {
@@ -29,47 +30,45 @@ buttons_conf_esp32 = {
     26: {
         "name": "1",
         "led_out": 16,
-        "commands": {
-            "Alarm<img>{}".format(tv): "alarm",
-            "Starta repl<img>{}".format(poop): "func:start_web_repl",
-        },
+        "commands": OrderedDict([
+            ("Alarm<img>{}".format(tv), "alarm"),
+            ("Starta repl<img>{}".format(poop), "func:start_web_repl")]),
         "enabled": True,
     },
     25: {"name": "2", "led_out": 19, "commands": {}, "enabled": False},
     33: {
         "name": "3",
         "led_out": 17,
-        "commands": {"Children TV": "children_tv",
-                     "TV4": "tv4",
-                     "Fire": "fire",
-                     "Party": "party"},
+        "commands": OrderedDict([("Children TV", "children_tv"),
+                      ("TV4", "tv4"),
+                      ("Fire", "fire"),
+                      ("Party", "party")]),
         "enabled": True,
     },
     32: {
         "name": "4",
         "led_out": 27,
-        "commands": {"Knapp 4": "button4_led_always_on"},
+        "commands": OrderedDict([("Knapp 4", "button4_led_always_on")]),
         "enabled": True,
     },
     35: {"name": "5-not-working", "led_out": 2, "commands": {}, "enabled": False},
     34: {
         "name": "6",
         "led_out": 18,
-        "commands": {"Lights off": "lights_off",
-                     "Lights on": "lights_on"},
+        "commands": OrderedDict([("Lights off", "lights_off"),
+                                 ("Lights on", "lights_on")]),
         "enabled": True,
     },
     39: {
         "name": "7",
         "led_out": 23,
-        "commands": {
-            "All off": "all_off",
-            "Low volume": "low_volume",
-            "Medium volume": "medium_volume",
-            "High volume": "high_volume",
-            "Group audio": "group_audio",
-            "Ungroup audio": "ungroup_audio"
-        },
+        "commands": OrderedDict([
+            ("All off", "all_off"),
+            ("Low volume", "low_volume"),
+            ("Medium volume", "medium_volume"),
+            ("High volume", "high_volume"),
+            ("Group audio", "group_audio"),
+            ("Ungroup audio": "ungroup_audio")]),
         "enabled": True,
     },
 }
